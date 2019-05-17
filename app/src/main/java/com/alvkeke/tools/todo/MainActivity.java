20b8f7b03e6341924c10eaa907851b21c7563d29
alvkeke <alvkeke@126.com>
2019-05-17 19:32:01 +0800

    add feature:add project

--- app/src/main/java/com/alvkeke/tools/todo/MainActivity.java
+++ app/src/main/java/com/alvkeke/tools/todo/MainActivity.java
@@ -323,6 +323,7 @@ public class MainActivity extends AppCompatActivity {
                     projects.add(new Project(id, proName, color));
                 }
                 proAdapter.notifyDataSetChanged();
+                exitProjectSettingMode();
             //}
         }
     }
